c606766505764b3708a9186fcdc1c61ec930e6e7
haagen24 <40901301+haagen24@users.noreply.github.com>
2024-02-05 02:46:44 -0500

    configure knexfile for jawsdb

--- knexfile.mjs
+++ knexfile.mjs
@@ -2,13 +2,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const dbUrl = process.env.JAWSDB_URL;
+const dbConfig = new URL(dbUrl);
+
 const knexConfig = {
     client: "mysql2",
     connection: {
-        host: process.env.DB_HOST,
-        database: process.env.DB_LOCAL_NAME,
-        user: process.env.DB_LOCAL_USER,
-        password: process.env.DB_LOCAL_PASSWORD,
+        host: dbConfig.hostname || process.env.DB_HOST,
+        database:
+            dbConfig.pathname.replace("/", "") || process.env.DB_LOCAL_NAME,
+        user: dbConfig.username || process.env.DB_LOCAL_USER,
+        password: dbConfig.password || process.env.DB_LOCAL_PASSWORD,
         charset: "utf8",
     },
     migrations: {
